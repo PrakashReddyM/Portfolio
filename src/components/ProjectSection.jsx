7af2d1d0f066b8af0080ae2be055fb92613f09a1
Prakash Reddy <125439176+PrakashReddyM@users.noreply.github.com>
2024-07-23 07:42:50 +0530

    Update ProjectSection.jsx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -9,18 +9,21 @@ const ProjectSection = () => {
             <h2 className="text-center text-success font-weight-bold  mb-5">Projects</h2>
             <div className='col mt-5'>
                 <div className="car">
-                    <div><a href="https://mbachaiwalah.vercel.app/"><img src={require("../asswsts/pic1.png")} alt="pic1" /></a></div>
+                    <div><a href="https://tomato-dun.vercel.app/"><img src={require("../asswsts/tomato.png")} alt="pic1" /></a></div>
                     <div className='content'>
-                        <h4>MBA ChaiWalah</h4>
-                        <p>An immersive tea marketplace crafted with React and CSS, offering a seamless blend of tradition and modernity in UI design.</p>
+                        <h4>Tomato</h4>
+                        <p>Developed a full-featured Tomato food delivery application, completing the frontend 
+using React.js. Currently finalizing the backend with Node.js, Express, and MongoDB to 
+ensure a seamless user experience. </p>
                     </div>
                 </div>
                 <div className="car bus">
                     <div className='content'>
-                        <h4>Videos HUB</h4>
-                        <p>Explore and upload videos effortlessly,Designed with React and Material UI, featuring a sleek UI design for seamless navigation and engagement.</p>
+                        <h4>Shopper</h4>
+                        <p>Currently developing a comprehensive e-commerce website using the MERN stack. 
+Completed 30% of the project, including the basic frontend design.</p>
                     </div>
-                    <div><a href="https://react-video-app-eight-khaki.vercel.app/"><img src={require("../asswsts/pic2.png")} alt="pic2" /></a></div>
+                    <div><a href="https://shopper-d67no5tva-prakashreddyms-projects.vercel.app/"><img src={require("../asswsts/shopper.png")} alt="pic2" /></a></div>
                 </div>
                 <div className="car">
                     <div><a href="/"><img src={require("../asswsts/pic3.jpeg")} alt="pic3" /></a></div>
